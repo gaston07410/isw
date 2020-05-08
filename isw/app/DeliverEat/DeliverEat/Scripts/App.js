dfc5ec2b05130644b9d9d7918818942e5a33e5f2
MaxiAlbert <Maximilianoalbert96@gmail.com>
2020-05-08 01:13:30 -0300

    Agregado de codigo encargado de manejar la api de google maps, agregado de codigo complementario para la api

--- isw/app/DeliverEat/DeliverEat/Scripts/App.js
+++ isw/app/DeliverEat/DeliverEat/Scripts/App.js
@@ -1,1 +1,25 @@
-﻿
+﻿var marker;
+//Funcionalidad para el mapa//
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: { lat: -31.4199055, lng: -64.1887394 }
+    });
+// insertar marcador//
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: { lat: -31.4208233, lng: -64.1887394 }
+    });
+    marker.addListener('click', toggleBounce);
+}
+//movimiento del marcador//
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
